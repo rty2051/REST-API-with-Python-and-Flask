c1a16422b3dd7fe609b373c4ebcde68f38b7ad00
Ryan Yam <rty2051@rit.edu>
2024-12-24 14:30:25 -0500

    Finished tutorial.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask, url_for
 from flask import request
 from flask import json
 from flask import Response
+from functools import wraps
+from flask import jsonify
+
+
 
 app = Flask(__name__)
 
@@ -52,18 +56,79 @@ def api_message():
     else:
         return "415 Unsupported Media Type ;)"
     
-@app.route('/hello', methods = ['GET'])
-def api_hello():
-    data = {
-        'hello'   :   'world',
-        'number'  :   3
-    }
-    js = json.dumps(data)
+# @app.route('/hello', methods = ['GET'])
+# def api_hello():
+#     data = {
+#         'hello'   :   'world',
+#         'number'  :   3
+#     }
+#     js = json.dumps(data)
 
-    resp = Response(js, status=200, mimetype='application/json')
-    resp.headers['Link'] = 'http://luisrei.com'
+#     # resp = Response(js, status=200, mimetype='application/json')
+#     resp = jsonify(data)
+#     resp.status_code = 200
+#     resp.headers['Link'] = 'http://luisrei.com'
+
+#     return resp
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message={
+        'status': 404,
+        'message': 'Not Found: ' + request.url
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
 
     return resp
 
+@app.route('/users/<userid>', methods=['GET'])
+def api_users(userid):
+    users = {'1':'john', '2':'steve','3':'bill'}
+    if userid in users:
+        return jsonify({userid:users[userid]})
+    else:
+        return not_found()
+
+def check_auth(username, password):
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    message = {'message': 'Authenticate.'}
+    resp = jsonify(message)
+
+    resp.status_code = 401
+    resp.headers['WWW-Authenticate'] = 'Basic realm="Example"'
+
+    return resp
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth:
+            return authenticate()
+        elif not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+# @app.route('/secrets')
+# @requires_auth
+# def api_hello():
+#     return "Shhh this is top secret spy stuff!"
+
+@app.route('/hello', methods = ['GET'])
+def api_hello():
+    app.logger.info('informing')
+    app.logger.warning('warning')
+    app.logger.error('screaming bloody murder!')
+    return "check your logs\n"
+
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(debug=True)
+    import logging
+    file_handler = logging.FileHandler('app.log')
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
